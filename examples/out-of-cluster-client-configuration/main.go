ce72bdaaae7e370ad88e2a8b4dbe5741f30b5d4f
Luis San Martin <luis@sanmartin.io>
2019-10-10 21:52:11 +0200

    add example to list all tls ingress

--- examples/out-of-cluster-client-configuration/main.go
+++ examples/out-of-cluster-client-configuration/main.go
@@ -22,9 +22,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"time"
 
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -59,32 +57,23 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	for {
-		pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
-		// Examples for error handling:
-		// - Use helper functions like e.g. errors.IsNotFound()
-		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		namespace := "default"
-		pod := "example-xxxxx"
-		_, err = clientset.CoreV1().Pods(namespace).Get(pod, metav1.GetOptions{})
-		if errors.IsNotFound(err) {
-			fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
-		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-			fmt.Printf("Error getting pod %s in namespace %s: %v\n",
-				pod, namespace, statusError.ErrStatus.Message)
-		} else if err != nil {
-			panic(err.Error())
-		} else {
-			fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
-		}
-
-		time.Sleep(10 * time.Second)
+	// we will list every ingress using tls. Why? to check for expiration date and warn
+	ingress, err := clientset.ExtensionsV1beta1().Ingresses("").List(metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
 	}
+
+	// there must be a better way!
+	for _, s := range ingress.Items {
+		for p := range s.Spec.TLS {
+			for _, h := range s.Spec.TLS[p].Hosts {
+				fmt.Println(h)
+			}
+		}
+
+	}
+
 }
 
 func homeDir() string {
