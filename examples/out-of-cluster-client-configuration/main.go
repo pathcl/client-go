b5634ed75887486f99abc8e55b6f6c803db244aa
Luis San Martin <luis@sanmartin.io>
2019-10-14 20:51:17 +0200

    simpler tls check

--- examples/out-of-cluster-client-configuration/main.go
+++ examples/out-of-cluster-client-configuration/main.go
@@ -18,17 +18,12 @@ limitations under the License.
 package main
 
 import (
-	"crypto/tls"
-	"crypto/x509"
 	"flag"
-	"fmt"
+	"log"
 	"os"
 	"path/filepath"
-	"strings"
-	"text/tabwriter"
-	"time"
 
-	"github.com/mitchellh/colorstring"
+	certs "github.com/hiroakis/go-check-ssl-certificates"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -42,35 +37,6 @@ import (
 	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 	// _ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
 )
-
-var (
-	days   int
-	months int
-	years  int
-)
-
-var sunsetSignatureAlgorithms = map[x509.SignatureAlgorithm]sunsetSignatureAlgorithm{
-	x509.MD2WithRSA: {
-		name: "MD2 with RSA",
-		date: time.Now(),
-	},
-	x509.MD5WithRSA: {
-		name: "MD5 with RSA",
-		date: time.Now(),
-	},
-	x509.SHA1WithRSA: {
-		name: "SHA1 with RSA",
-		date: time.Date(2017, 1, 1, 0, 0, 0, 0, time.UTC),
-	},
-	x509.DSAWithSHA1: {
-		name: "DSA with SHA1",
-		date: time.Date(2017, 1, 1, 0, 0, 0, 0, time.UTC),
-	},
-	x509.ECDSAWithSHA1: {
-		name: "ECDSA with SHA1",
-		date: time.Date(2017, 1, 1, 0, 0, 0, 0, time.UTC),
-	},
-}
 
 func main() {
 	var kubeconfig *string
@@ -98,46 +64,19 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-
-	now := time.Now()
-	twarn := now.AddDate(years, months, days)
-
-	hosts := hosts{}
-
+	var (
+		c *certs.Cert
+	)
 	// there must be a better way!
 	for _, s := range ingress.Items {
 		for p := range s.Spec.TLS {
 			for _, h := range s.Spec.TLS[p].Hosts {
-				certs, _ := checkHost(h, twarn)
-				hosts = append(hosts, host{name: h, certs: certs})
+				port := h + ":443"
+				c, _ = certs.ParseRemoteCertificate(port, 10)
+				log.Println(h, c.Jsonify())
 			}
 		}
-
 	}
-
-	w := tabwriter.NewWriter(os.Stdout, 20, 1, 2, ' ', 0)
-	fmt.Fprintln(w, "NAME\tSUBJECT\tISSUER\tALGO\tEXPIRES\tSUNSET DATE\tERROR")
-
-	// now we should iterate over hosts
-	for i := 0; i < len(hosts); i++ {
-		for _, cert := range hosts[i].certs {
-			sunset := ""
-			if cert.sunset != nil {
-				sunset = cert.sunset.date.Format("Jan 02, 2006")
-
-			}
-			expires := cert.expires
-			if cert.warn {
-				expires = colorstring.Color("[red]" + cert.expires + "[reset]")
-			}
-			error := cert.error
-			if error != "" {
-				error = colorstring.Color("[red]" + cert.error + "[reset]")
-			}
-			fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%s\t%s\n", cert.name, cert.subject, cert.issuer, cert.algo, expires, sunset, error)
-		}
-	}
-	w.Flush()
 }
 
 func homeDir() string {
@@ -145,107 +84,4 @@ func homeDir() string {
 		return h
 	}
 	return os.Getenv("USERPROFILE") // windows
-}
-
-type hosts []host
-
-func (h hosts) Len() int           { return len(h) }
-func (h hosts) Less(i, j int) bool { return h[i].name < h[j].name }
-func (h hosts) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-
-type host struct {
-	name  string
-	certs map[string]certificate
-}
-
-type certificate struct {
-	name    string
-	subject string
-	algo    string
-	issuer  string
-	expires string
-	warn    bool
-	error   string
-	sunset  *sunsetSignatureAlgorithm
-}
-
-type sunsetSignatureAlgorithm struct {
-	name string    // Human readable name of the signature algorithm.
-	date time.Time // Date the signature algorithm will be sunset.
-}
-
-func checkHost(h string, twarn time.Time) (map[string]certificate, error) {
-	if !strings.Contains(h, ":") {
-		// default to 443
-		h += ":443"
-	}
-	c, err := tls.Dial("tcp", h, nil)
-	if err != nil {
-		switch cerr := err.(type) {
-		case x509.CertificateInvalidError:
-			ht := createHost(h, twarn, cerr.Cert)
-			ht.error = err.Error()
-			return map[string]certificate{
-				string(cerr.Cert.Signature): ht,
-			}, nil
-		case x509.UnknownAuthorityError:
-			ht := createHost(h, twarn, cerr.Cert)
-			ht.error = err.Error()
-			return map[string]certificate{
-				string(cerr.Cert.Signature): ht,
-			}, nil
-		case x509.HostnameError:
-			ht := createHost(h, twarn, cerr.Certificate)
-			ht.error = err.Error()
-			return map[string]certificate{
-				string(cerr.Certificate.Signature): ht,
-			}, nil
-		}
-		return nil, fmt.Errorf("tcp dial %s failed: %v", h, err)
-	}
-	defer c.Close()
-
-	certs := make(map[string]certificate)
-	for _, chain := range c.ConnectionState().VerifiedChains {
-		for _, cert := range chain {
-			if _, checked := certs[string(cert.Signature)]; checked {
-				continue
-			}
-
-			ht := createHost(h, twarn, cert)
-
-			certs[string(cert.Signature)] = ht
-		}
-	}
-	return certs, nil
-}
-
-func createHost(name string, twarn time.Time, cert *x509.Certificate) certificate {
-	host := certificate{
-		name:    name,
-		subject: cert.Subject.CommonName,
-		issuer:  cert.Issuer.CommonName,
-		algo:    cert.SignatureAlgorithm.String(),
-	}
-
-	// check the expiration
-	if twarn.After(cert.NotAfter) {
-		host.warn = true
-	}
-	expiresIn := int64(time.Until(cert.NotAfter).Hours())
-	if expiresIn <= 48 {
-		host.expires = fmt.Sprintf("%d hours", expiresIn)
-	} else {
-		host.expires = fmt.Sprintf("%d days", expiresIn/24)
-	}
-
-	// Check the signature algorithm, ignoring the root certificate.
-	if alg, exists := sunsetSignatureAlgorithms[cert.SignatureAlgorithm]; exists {
-		if cert.NotAfter.Equal(alg.date) || cert.NotAfter.After(alg.date) {
-			host.warn = true
-		}
-		host.sunset = &alg
-	}
-
-	return host
 }
--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/googleapis/gnostic v0.0.0-20170729233727-0c5108395e2d
 	github.com/gophercloud/gophercloud v0.1.0
 	github.com/gregjones/httpcache v0.0.0-20170728041850-787624de3eb7
+	github.com/hiroakis/go-check-ssl-certificates v0.0.0-20170621095458-81dab13bd380 // indirect
 	github.com/imdario/mergo v0.3.5
 	github.com/peterbourgon/diskv v2.0.1+incompatible
 	github.com/spf13/pflag v1.0.3
